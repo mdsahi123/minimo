167cf59f0f9e747f791074790a15321557c0b116
MD SAHI ALAM KHAN <ksahi4088@gmail.com>
2022-06-26 12:29:51 +0530

    All file Uploaded

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// BX Slider
+$(document).ready(function(){
+    $('.slider').bxSlider({
+       auto: 'true',
+       pause: '5000',
+       speed: '800',
+       autostart: 'true',
+    });
+  });
